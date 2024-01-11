a48274b4443dbd59b27e0d5d193e09820896ef92
INTERNAL\christiaan.hemerik <christiaan.hemerik@ecare.nl>
2024-01-11 13:47:06 +0100

    Implements Tracker component in PassData

--- client/src/components/PassData.tsx
+++ client/src/components/PassData.tsx
@@ -1,30 +1,8 @@
 import { Fragment } from 'react';
 import { Pass } from '../types';
 import { downlinkData } from '../data';
-
-const getDateTime = (timestamp: number) => {
-  const today = new Date(Date.now()).toLocaleDateString('nl-NL');
-  const dateTime = new Date(timestamp * 1000).toLocaleString('nl-NL').split(' ');
-
-  if (dateTime[0] === today) {
-    return ['Today', dateTime[1]];
-  }
-  return dateTime;
-};
-
-const getDurationMinutesSeconds = (startTimestamp: number, endTimestamp: number) => {
-  const duration = endTimestamp - startTimestamp;
-  const minutes = Math.floor(duration / 60);
-  const seconds = parseInt(((duration % 60) / 1).toFixed(0));
-
-  return seconds === 60
-    ? `${minutes + 1} m`
-    : `${minutes} m ${seconds} s`;
-};
-
-const roundAzimuth = (azimuth: string | number) => {
-  return Math.round(typeof azimuth === 'number' ? azimuth : parseInt(azimuth));
-};
+import { getDateTime, getDurationMinutesSeconds, roundAzimuth } from '../utils';
+import { Tracker } from './Tracker';
 
 interface PassDataProps {
   id: string;
@@ -51,6 +29,7 @@ function PassData({ id, data, onSelect, isVisible }: PassDataProps) {
               </Fragment>
             ))}
           </dl>
+          <Tracker passes={data} />
         </div>
       )}
 
