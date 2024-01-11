cf456d84769a32cf54af21ebcba28333d10979c7
INTERNAL\christiaan.hemerik <christiaan.hemerik@ecare.nl>
2024-01-11 13:46:33 +0100

    Adds utils

---
+++ client/src/utils.ts
@@ -0,0 +1,27 @@
+export function getDateTime(timestamp: number) {
+  const today = new Date(Date.now()).toLocaleDateString('nl-NL');
+  const dateTime = new Date(timestamp * 1000).toLocaleString('nl-NL').split(' ');
+
+  if (dateTime[0] === today) {
+    return ['Today', dateTime[1]];
+  }
+  return dateTime;
+}
+
+export function getDurationMinutesSeconds(startTimestamp: number, endTimestamp: number) {
+  const duration = endTimestamp - startTimestamp;
+  const minutes = Math.floor(duration / 60);
+  const seconds = parseInt(((duration % 60) / 1).toFixed(0));
+
+  return seconds === 60
+    ? `${minutes + 1} m`
+    : `${minutes} m ${seconds} s`;
+}
+
+export function roundAzimuth(azimuth: string | number) {
+  return Math.round(typeof azimuth === 'number' ? azimuth : parseInt(azimuth));
+}
+
+export function getPercentage(x: number, y: number, fixed?: number): number {
+  return Number(((x / y) * 100).toFixed(fixed));
+}
